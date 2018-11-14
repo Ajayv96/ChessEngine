4e13ce7392e581c004a2e5bb2baa47275dfb1d1b
Ajay V <ajayvenkideswaran.mec@gmail.com>
2018-11-14 19:48:02 +0530

    Added a new initialisation file and updated variables

--- header.h
+++ header.h
@@ -3,14 +3,16 @@
 
 typedef unsigned long long UL;
 
-#define NAME "Vice 1.0"
+#define NAME "V 1.0"
 #define BRD_SQ_NUM 120
-
+#define MAXGAMEMOVES 2048
 //enum for each piece
 enum {EMPTY,wP,wN,wB,wR,wQ,wK,bP,bN,bB,bR,bQ,bK};
 enum {FILE_A,FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G,FILE_H, FILE_NONE};
 enum {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE};
 enum {WHITE,BLACK,BOTH};
+//Castling
+enum {WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8};
 enum {FALSE, TRUE};
 enum {
 A1 =21,B1,C1,D1,E1,F1,G1,H1,
@@ -23,14 +25,44 @@ A7 =81,B7,C7,D7,E7,F7,G7,H7,
 A8 =91,B8,C8,D8,E8,F8,G8,H8,NO_SQ
 };
 
+
 typedef struct 
 {
     int pieces[BRD_SQ_NUM];
-    UL pawns[3]; /* Each bit represents a pawn (1 - Present, 0 - Not Present)
-                  * pawns[0] - represent wp
-                  * pawns[1] - represents bp
-                  * pawns[2] - represents both
-                  */
+    UL pawns[3]; // Each bit represents a pawn (1 - Present, 0 - Not Present)
+    int KingSq[2]; // Location of both king
+    int side; //Which side is playing
+    int enPas;
+    int fiftyMove;
+    int ply; // Half move count
+    int hisPly; 
+    int castlePerm;    
+    UL posKey;
+    int pceNum[13]; //Number of pieces on the board
+    int bigPce[3]; // Non pawn pieces
+    int majPce[3]; // King queen
+    int minPce[3]; //Rooks and bishops
+    S_UNDO history[MAXGAMEMOVES]; // Details before the move was made - repetition of moves
 } S_BOARD;
+
+typedef struct {
+
+    int move;
+    int castlePerm;
+    int enpas;
+    int fiftyMove;
+    UL posKey;
+} S_UNDO;  // Holds undo information
+
+/* Macros */
+
+/* Globals */
+
+extern int Sq120ToSq64[BRD_SQ_NUM];
+extern int Sq64ToSq120[64];
+
+/* Functions */
+
+
 
 #endif
